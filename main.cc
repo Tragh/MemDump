f25dccd40ae69b737b91c268a8b08bad3adf6945
Tragh <mikemth@gmail.com>
2016-04-09 23:49:22 +0100

    First commit

---
+++ main.cc
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <regex>
+#include <iterator>
+#include <cassert>
+#include <inttypes.h>
+
+struct MapsLine{
+	bool valid;
+	uint64_t start;
+	uint64_t end;
+	std::string permissions;
+	uint64_t offset;
+	std::string device;
+	uint64_t inode;
+	std::string pathname;
+	
+	MapsLine(const MapsLine&)=default;
+	MapsLine(std::string line){
+		//regex to split each line of the maps file
+		std::regex re("^([^\\s-]+)-([^\\s]+)\\s([^\\s]+)\\s([^\\s]+)\\s([^\\s]+)\\s([^\\s]+)\\s*(.*)$");
+		std::smatch sm;
+		
+		//match and store matches in sm
+		std::regex_match(line,sm,re);
+		
+		//it should split into 8 parts
+		//the first is just the whole string
+		if(sm.size()!=8){
+			valid=false;
+			return;
+		}
+		
+		//assign each part to a member variable
+		start=std::stoull(sm[1],0,16);
+		end=std::stoull(sm[2],0,16);
+		permissions=sm[3];
+		offset=std::stoull(sm[4],0,16);
+		device=sm[5];
+		inode=std::stoull(sm[6]);
+		pathname=sm[7];
+		valid=true;
+	}
+	
+	std::string as_string(){
+		std::stringstream ss;
+		ss << std::hex << start << "-" << end << " " 
+			<< permissions << " "
+			<< offset << " "
+			<< device << " "
+			<< std::dec << inode << " "
+			<< pathname;
+		return ss.str();
+	}
+};
+
+
+int main(){
+	
+	int PID;
+	std::cout << "Enter PID:" << std::endl;
+	std::cin >> PID;
+
+	std::vector<MapsLine> maps;
+
+	{
+		//open the memory maping file for the PID
+		std::ifstream infile("/proc/"+std::to_string(PID)+"/maps");
+		if(!infile){
+			std::cout << "Couldn't open process. Wrong PID? Aborting." << std::endl;
+			return 0;
+		}
+		std::string line;
+		while (std::getline(infile, line)){
+			//parse each line of the file
+			MapsLine m(line);
+			if(!m.valid){
+				std::cout << "Error parsing line in /proc/PID/maps file" << std::endl;
+				return 0;
+			}
+			if(m.inode==0)
+				maps.push_back(m); //and store in vector
+		}
+	}
+	
+	std::cout << "Select a region to dump:" << std::endl << std::endl;
+	for(int i=0;i<maps.size();++i){
+		std::cout << "[" << i << "] " <<  maps[i].as_string() << std::endl;
+	}
+
+	std::cout << std::endl << std::endl << "Select a region to dump:" << std::endl;
+	
+	int region;
+	std::cin >> region;
+	assert((region>=0) && (region < maps.size()));
+
+	std::cout << "This will create a file: dump.bin in the current directory. \n"
+		<< "its size will be " << (maps[region].end-maps[region].start) << " bytes. \n"
+		<< "Continue? [y/N]" << std::endl;
+	
+	std::string yn;
+	std::cin >> yn;
+	if(yn.size()<0 || !(yn[0]=='Y' || yn[0]=='y')){
+		std::cout << "Aborting." << std::endl;
+		return 0;
+	}
+
+	//open output file
+	std::ofstream outfile ("dump.bin",std::ios::binary);
+	if(!outfile){
+		std::cout << "Couldn't open output file. Aborting." << std::endl;
+		return 0;
+	}
+	
+	//open process memory
+	std::ifstream infile ("/proc/"+std::to_string(PID)+"/mem",std::ios::binary);
+	if(!infile){
+		std::cout << "Couldn't open process memory. Aborting." << std::endl;
+		return 0;
+	}
+	
+	//seek to the correct position
+	infile.seekg(maps[region].start);
+	
+	//create istreambuf iterator (starts at the place seeked to)
+	std::istreambuf_iterator<char> infile_it(infile);
+	std::ostreambuf_iterator<char> outfile_it(outfile);
+	
+	//copy the correct number of bytes to the outfile
+	std::copy_n(infile_it, maps[region].end-maps[region].start, outfile_it );
+	
+	std::cout << "Done!" << std::endl;
+	       
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -std=gnu++11 -march=native -O2 -pipe -fomit-frame-pointer main.cc -o main
